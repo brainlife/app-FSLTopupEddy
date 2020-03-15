a415f2499d62cda353074809ea5366cd6b6f1c02
Brad Caron <bacaron@iu.edu>
2020-03-15 14:31:08 -0400

    Reason: expose topup and eddy params. move to cuda

--- topup_eddy.sh
+++ topup_eddy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 ## This app will combine opposite-encoding direction DWI images and perform eddy and motion
 ## correction using FSL's topup and eddy_openmp commands.
 ## Jesper L. R. Andersson, Mark S. Graham, Eniko Zsoldos and Stamatios N. Sotiropoulos. Incorporating outlier detection and replacement into a non-parametric framework for movement 
@@ -15,6 +17,8 @@
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/lib/nvidia-410:$LD_LIBRARY_PATH
 
+ln -sf ${FSLDIR}/bin/eddy_cuda8.0 ${FSLDIR}/bin/eddy_cuda
+
 ## File paths
 diff=`jq -r '.diff' config.json`;
 bvec=`jq -r '.bvec' config.json`;
@@ -22,8 +26,57 @@ bval=`jq -r '.bval' config.json`;
 rdif=`jq -r '.rdif' config.json`;
 rbvc=`jq -r '.rbvc' config.json`;
 rbvl=`jq -r '.rbvl' config.json`;
+
+# topup options
 param_num=`jq -r '.param' config.json`;
 encode_dir=`jq -r '.encode' config.json`;
+warpres=`jq -r '.warpres' config.json`;
+subsamp=`jq -r '.subsamp' config.json`;
+fwhm=`jq -r '.fwhm' config.json`;
+miter=`jq -r '.miter' config.json`;
+lambda=`jq -r '.lambda' config.json`;
+ssqlambda=`jq -r '.ssqlambda' config.json`;
+regmod=`jq -r '.regmod' config.json`;
+estmov=`jq -r '.estmov' config.json`;
+minmet=`jq -r '.minmet' config.json`;
+splineorder=`jq -r '.splineorder' config.json`;
+numprec=`jq -r '.numprec' config.json`;
+interp=`jq -r '.interp' config.json`;
+scale=`jq -r '.scale' config.json`;
+regrid=`jq -r '.regrid' config.json`;
+
+# eddy options
+mb=`jq -r '.mb' config.json`;
+mb_offs=`jq -r '.mb_offs' config.json`;
+flm=`jq -r '.flm' config.json`;
+slm=`jq -r '.slm' config.json`;
+eddy_fwhm=`jq -r '.eddy_fwhm' config.json`;
+eddy_niter=`jq -r '.eddy_niter' config.json`;
+fep=`jq -r '.fep' config.json`;
+eddy_interp=`jq -r '.eddy_interp' config.json`;
+eddy_resamp=`jq -r '.eddy_resamp' config.json`;
+nvoxhp=`jq -r '.nvoxhp' config.json`;
+initrand=`jq -r '.initrand' config.json`;
+ff=`jq -r '.ff' config.json`;
+repol=`jq -r '.repol' config.json`;
+resamp=`jq -r '.resamp' config.json`;
+ol_nstd=`jq -r '.ol_nstd' config.json`;
+ol_nvox=`jq -r '.ol_nvox' config.json`;
+ol_type=`jq -r '.ol_type' config.json`;
+ol_pos=`jq -r '.ol_pos' config.json`;
+ol_sq=`jq -r '.ol_sq' config.json`;
+mporder=`jq -r '.mporder' config.json`;
+s2v_niter=`jq -r '.s2v_niter' config.json`;
+s2v_lambda=`jq -r '.s2v_lambda' config.json`;
+s2v_interp=`jq -r '.s2v_interp' config.json`;
+estimate_move_by_susceptibility=`jq -r '.estimate_move_by_susceptibility' config.json`;
+mbs_niter=`jq -r '.mbs_niter' config.json`;
+mbs_lambda=`jq -r '.mbs_lambda' config.json`;
+mbs_ksp=`jq -r '.mbs_ksp' config.json`;
+dont_peas=`jq -r '.dont_peas' config.json`;
+data_is_shelled=`jq -r '.data_is_shelled' config.json`;
+
+# phase dirs
 phase="diff rdif"
 
 ## Create folder structures
@@ -120,6 +173,18 @@ else
 	fi
 fi
 
+if [[ ${scale} == 'true' ]]; then
+	scale=1
+else
+	scale=0
+fi
+
+if [[ ${regrid} == 'true' ]]; then
+	regrid=1
+else
+	regrid=0
+fi
+
 ## setting up top-up for susceptibility correction
 if [ -f my_unwarped_images.nii.gz ];
 then
@@ -128,10 +193,23 @@ else
 	echo "topup"
 	topup --imain=b0_images.nii.gz \
 	      --datain=acq_params.txt \
-	      --config=b02b0.cnf \
 	      --out=my_topup_results \
 	      --fout=my_field \
-	      --iout=my_unwarped_images;
+	      --iout=my_unwarped_images\
+	      --warpres=${warpres} \
+	      --subsamp=${subsamp} \
+	      --fwhm=${fwhm} \
+	      --miter=${miter} \
+	      --lambda=${lambda} \
+	      --ssqlambda=${ssqlambda} \
+	      --regmod=${regmod} \
+	      --estmov=${estmov} \
+	      --minmet=${minmet} \
+	      --splineorder=${splineorder} \
+	      --numprec=${numprec} \
+	      --interp=${interp} \
+	      --scale=${scale} \
+	      --regrid=${regrid};
 fi
 
 ## Averaging b0 images from topup
@@ -191,21 +269,98 @@ else
 	echo $indx > index.txt;
 fi
 
+# parse parameters for eddy that are set as flags only
+if [[ ${fep} == true ]]; then
+	fep="--fep "
+else
+	fep=""
+fi
+
+if [[ ${repol} == true ]]; then
+	repol="--repol "
+else
+	repol=""
+fi
+
+if [[ ${dont_sep_offs_move} == true ]]; then
+	dont_sep_offs_move="--dont_sep_offs_mov"
+else
+	dont_sep_offs_move=""
+fi
+
+if [[ ${dont_peas} == true ]]; then
+	dont_peas="--dont_peas"
+else
+	dont_peas=""
+fi
+
+if [[ ${ol_pos} == true ]]; then
+	ol_pos="--ol_pos"
+else
+	ol_pos=""
+fi
+
+if [[ ${ol_sqr} == true ]]; then
+	ol_sqr="--ol_sqr"
+else
+	ol_sqr=""
+fi
+
+if [[ ${estimate_move_by_susceptibility} == true ]]; then
+	estimate_move_by_susceptibility="--estimate_move_by_susceptibility"
+else
+	estimate_move_by_susceptibility=""
+fi
+
+if [[ ${data_is_shelled} == true ]]; then
+	data_is_shelled="--data_is_shelled"
+else
+	data_is_shelled=""
+fi
+
+
 ## Eddy correction
 if [ -f eddy_corrected_data.nii.gz ];
 then
 	echo "eddy completed. skipping"
 else
 	echo "eddy"
-	eddy_openmp --imain=data \
+	eddy_cuda --imain=data \
 		--mask=my_unwarped_images_avg_brain_mask \
 		--acqp=acq_params.txt \
 		--index=index.txt \
 		--bvecs=bvecs \
 		--bvals=bvals \
 		--topup=my_topup_results \
-		--repol \
-		--out=eddy_corrected_data;
+		--out=eddy_corrected_data \
+		--flm=${flm} \
+		--slm=${slm} \
+		--fwhm=${eddy_fwhm} \
+		--niter=${eddy_niter} \
+		${fep} \
+		--interp=${eddy_interp} \
+		--resamp=${resamp} \
+		--nvoxhp=${nvoxhp} \
+		--ff=${ff} \
+		${dont_sep_offs_move} \
+		${dont_peas} \
+		${repol} \
+		--ol_nstd=${ol_nstd} \
+		--ol_nvox=${ol_vox} \
+		--ol_type=${ol_type} \
+		${ol_pos} \
+		${ol_sqr} \
+		--mb=${mb} \
+		--mb_offs=${mb_offs} \
+		--mporder=${mporder} \
+		--s2v_niter=${s2v_niter} \
+		--s2v_lambda=${s2v_lambda} \
+		--s2v_interp=${s2v_interp} \
+		${estimate_move_by_susceptibility} \
+		--mbs_niter=${mbs_niter} \
+		--mbs_lambda=${mbs_lambda} \
+		--mbs_kps=${mbs_kps} \
+		${data_is_shelled};
 fi
 
 ## brain extraction on combined data image
@@ -223,7 +378,7 @@ mv eddy_corrected_data.nii.gz ./dwi/dwi.nii.gz;
 mv eddy_corrected_data.eddy_rotated_bvecs ./dwi/dwi.bvecs;
 mv bvals ./dwi/dwi.bvals;
 mv eddy_corrected_brain_mask.nii.gz ./mask/mask.nii.gz;
-rm -rf *eddy_corrected* index.txt *my_* *b0_images* acq_params.txt diff rdif data.nii.gz bvecs;
+#rm -rf *eddy_corrected* index.txt *my_* *b0_images* acq_params.txt diff rdif data.nii.gz bvecs;
 
 
 
